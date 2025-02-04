e1cfb708f9025dcb80347dcd6fff9ab8c0d2b986
harryadav3 <03yadavanurag@gmail.com>
2025-02-04 23:20:03 +0530

    added general simple text

--- martin.py
+++ martin.py
@@ -98,6 +98,164 @@ class MultiHeadAttention(nn.Module):
         return context_vec
 
 
+class LayerNorm(nn.Module):
+    
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+        
+    
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(d_in=cfg["emb_dim"], d_out=cfg["emb_dim"], context_lenght=cfg["context_length"],num_heads=cfg["n_heads"], dropout=cfg["dropout"], qkv_bias=cfg["num_heads"])
+    
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["dropout"])
+
+    def forward(self,x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut 
+        
+        
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        
+        return x
+    
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.token_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(*[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.final_norm(x)
+        x = self.trf_blocks(x)
+        logits = self.out_head(x)
+        return logits
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+
+    for _ in range(max_new_tokens):
+
+        
+        idx_cond = idx[:, -context_size:]
+        
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        logits = logits[:, -1, :]
+        
+        idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        idx = torch.cat([idx, idx_next], dim=-1)
+        
+    return idx
+
+
+def main():
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,
+        "emb_dim": 768,
+        "context_length": 1024,
+        "n_layers": 12,
+        "n_heads": 12,
+        "drop_rate": 0.1,
+        "qkb_bias": False
+    }
+
+    
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    model.eval()
+    
+    start_context = "Hello, I am"
+    
+    tokenizer = tiktoken.get_encoding("gpt2")
+    encoded = tokenizer.encode(start_context)
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    
+    print(f"\n{50*'='}\n{22*' '}IN\n{50*'='}\n")
+    print("\nInput text:", start_context)
+    print("Encoded input text:", encoded)
+    print("encoded_tensor shape:", encoded_tensor.shape)
+    
+    
+    out = generate_text_simple(
+        model=model,
+        idx=encoded_tensor,
+        max_new_tokens=10,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+
+    decoded_text = tokenizer.decode(out[0].tolist())
+
+    print(f"\n\n{50*'='}\n{22*' '}OUT\n{50*'='}\n")
+    print(f"\nOutput:", out)
+    print("Output lenght:", len(out[0]))
+    print("Output text:", decoded_text)
+
+    
+if __name__ == "__main__":
+    main()
+    
+    
+
 
 
     
