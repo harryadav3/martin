fixed printing gradient accumulation and adding input.txt

reproduce_gpt.py
@@ -341,12 +341,15 @@ for step in range(50):
 
     t0 = time.time()
     optimizer.zero_grad()
+    loss_accum = 0.0
     for micro_step in range(grad_accum_steps):
         x, y = data_loader.next_batch()
         x, y = x.to(device), y.to(device)
         with torch.autocast(device_type=device, dtype=torch.bfloat16):
             logits, loss = model(x, y)
         loss = loss / grad_accum_steps # scale the loss by the number of accumulation steps
+
+        loss_accum += loss.detach()
         loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # gradient clipping
 
@@ -357,9 +360,10 @@ for step in range(50):
 
     torch.cuda.synchronize() #wait for the gpu operation 
     t1 = time.time()
-    dt = ( t1 - t0 ) * 1000
-    tokens_per_sec = (data_loader.B * data_loader.T) / (t1 - t0)
-    print(f"step {step:4d} | loss: {loss.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} |  dt: {dt:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+    dt = ( t1 - t0 ) 
+    tokens_processed = data_loader.B * data_loader.T * grad_accum_steps
+    tokens_per_sec = tokens_processed / dt
+    print(f"step {step:4d} | loss: {loss_accum.item():.6f} | lr {lr:.4e} | norm: {norm:.4f} |  dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 
 import sys; sys.exit(0)